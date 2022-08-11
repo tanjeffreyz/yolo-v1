d793a33a67f62e53a23671f7cab5ad8eeddb76c3
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-08-11 16:11:07 -0700

    code cleanup

--- models.py
+++ models.py
@@ -10,17 +10,17 @@ class YOLOv1(nn.Module):
 
         layers = [
             # Probe(0, forward=lambda x: print('#' * 5 + ' Start ' + '#' * 5)),
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),  # Conv 1
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),                   # Conv 1
             nn.LeakyReLU(negative_slope=0.1),
             # Probe('conv1', forward=probe_dist),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
-            nn.Conv2d(64, 192, kernel_size=3, padding=1),          # Conv 2
+            nn.Conv2d(64, 192, kernel_size=3, padding=1),                           # Conv 2
             nn.LeakyReLU(negative_slope=0.1),
             # Probe('conv2', forward=probe_dist),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
-            nn.Conv2d(192, 128, kernel_size=1),         # Conv 3
+            nn.Conv2d(192, 128, kernel_size=1),                                     # Conv 3
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.Conv2d(256, 256, kernel_size=1),
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
@@ -29,7 +29,7 @@ class YOLOv1(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2)
         ]
 
-        for i in range(4):                              # Conv 4
+        for i in range(4):                                                          # Conv 4
             layers += [
                 nn.Conv2d(512, 256, kernel_size=1),
                 nn.Conv2d(256, 512, kernel_size=3, padding=1)
@@ -42,7 +42,7 @@ class YOLOv1(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2)
         ]
 
-        for i in range(2):                              # Conv 5
+        for i in range(2):                                                          # Conv 5
             layers += [
                 nn.Conv2d(1024, 512, kernel_size=1),
                 nn.Conv2d(512, 1024, kernel_size=3, padding=1)
@@ -54,18 +54,18 @@ class YOLOv1(nn.Module):
             # Probe('conv5', forward=probe_dist),
         ]
 
-        for _ in range(2):                              # Conv 6
+        for _ in range(2):                                                          # Conv 6
             layers.append(nn.Conv2d(1024, 1024, kernel_size=3, padding=1))
         layers.append(nn.LeakyReLU(negative_slope=0.1))
         # layers.append(Probe('conv6', forward=probe_dist))
 
         layers += [
             nn.Flatten(),
-            nn.Linear(config.S * config.S * 1024, 4096),            # Linear 1
+            nn.Linear(config.S * config.S * 1024, 4096),                            # Linear 1
             nn.Dropout(),
             nn.LeakyReLU(negative_slope=0.1),
             # Probe('linear1', forward=probe_dist),
-            nn.Linear(4096, config.S * config.S * self.depth),       # Linear 2
+            nn.Linear(4096, config.S * config.S * self.depth),                      # Linear 2
             # Probe('linear2', forward=probe_dist),
         ]
 
