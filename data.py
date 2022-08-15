moved preprocess out of dataset, added batch size to tests, fixed exploding gradient by dividing loss by batch size

data.py
@@ -14,30 +14,35 @@ class YoloPascalVocDataset(Dataset):
 
     def __init__(self, folder):
         assert folder in {'train', 'test'}
-        target = os.path.join('data', folder)
-        self.classes = utils.load_class_dict()
+        self.files = glob.glob(os.path.join('data', folder, '*'))
+
+    def __getitem__(self, i):
+        return torch.load(self.files[i])
+
+    def __len__(self):
+        return len(self.files)
+
+
+def preprocess(folder):
+    classes = utils.load_class_dict()
+    dataset = VOCDetection(
+        root=config.DATA_PATH,
+        year='2007',
+        image_set=('train' if folder == 'train' else 'val'),
+        download=True,
+        transform=T.Compose([
+            T.ToTensor(),
+            T.Resize(config.IMAGE_SIZE)
+        ])
+    )
+
+    output_dir = os.path.join(config.DATA_PATH, folder)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for i, data_pair in enumerate(tqdm(dataset, desc=f'Preprocessing {folder}')):
+        target = os.path.join(output_dir, str(i))
         if not os.path.exists(target):
-            self.preprocess(folder)
-            utils.save_class_dict(self.classes)
-        self.files = glob.glob(os.path.join(target, '*'))
-
-    def preprocess(self, folder):
-        dataset = VOCDetection(
-            root=config.DATA_PATH,
-            year='2007',
-            image_set=('train' if folder == 'train' else 'val'),
-            download=True,
-            transform=T.Compose([
-                T.ToTensor(),
-                T.Resize(config.IMAGE_SIZE)
-            ])
-        )
-
-        output_dir = os.path.join(config.DATA_PATH, folder)
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
-        for i, data_pair in enumerate(tqdm(dataset, desc=f'Preprocessing {folder}')):
             data, label = data_pair
             grid_size_x = data.size(dim=2) / config.S       # Images in PyTorch have size (channels, height, width)
             grid_size_y = data.size(dim=1) / config.S
@@ -48,10 +53,10 @@ class YoloPascalVocDataset(Dataset):
             ground_truth = torch.zeros((config.S, config.S, depth))
             for j, bbox_pair in enumerate(utils.get_bounding_boxes(label)):
                 name, coords = bbox_pair
-                if name not in self.classes:
-                    self.classes[name] = YoloPascalVocDataset.index
+                if name not in classes:
+                    classes[name] = YoloPascalVocDataset.index
                     YoloPascalVocDataset.index += 1
-                class_index = self.classes[name]
+                class_index = classes[name]
                 x_min, x_max, y_min, y_max = coords
                 mid_x = (x_max + x_min) / 2
                 mid_y = (y_max + y_min) / 2
@@ -82,20 +87,20 @@ class YoloPascalVocDataset(Dataset):
                 ground_truth[row, col, -config.C:] = one_hot
 
             # Save preprocessed data
-            torch.save((data, ground_truth), os.path.join(output_dir, str(i)))
-
-    def __getitem__(self, i):
-        return torch.load(self.files[i])
-
-    def __len__(self):
-        return len(self.files)
+            torch.save((data, ground_truth), target)
+    utils.save_class_dict(classes)
 
 
 if __name__ == '__main__':
-    classes = utils.load_class_array()
+    # Preprocess data
+    preprocess('train')
+    preprocess('test')
+
+    # Display data
+    obj_classes = utils.load_class_array()
     train_set = YoloPascalVocDataset('train')
     num_negatives = 0
     for data, label in train_set:
         num_negatives += torch.sum(label < 0).item()
-        utils.plot_boxes(data, label, classes)
+        utils.plot_boxes(data, label, obj_classes)
     print(num_negatives)

tests.py
@@ -18,7 +18,7 @@ class TestModel(unittest.TestCase):
 
 
 class TestLossFunction(unittest.TestCase):
-    SHAPE = (config.S, config.S, 5 * config.B + config.C)
+    SHAPE = (config.BATCH_SIZE, config.S, config.S, 5 * config.B + config.C)
 
     def test_zeros(self):
         test = torch.zeros(TestLossFunction.SHAPE)
@@ -45,10 +45,10 @@ class TestLossFunction(unittest.TestCase):
 
     def test_single_bbox(self):
         truth = torch.zeros(TestLossFunction.SHAPE)
-        truth[0, 0, 4] = 1.0        # Bbox confidence
-        truth[0, 0, -1] = 1.0       # Class
+        truth[0, 0, 0, 4] = 1.0        # Bbox confidence
+        truth[0, 0, 0, -1] = 1.0       # Class
         pred = torch.zeros(TestLossFunction.SHAPE)
-        pred[0, 0, 0:5] = torch.ones(5)
+        pred[0, 0, 0, 0:5] = torch.ones(5)
         loss_func = SumSquaredErrorLoss()
         result = loss_func(pred, truth)
         self.assertEqual(tuple(result.size()), ())
@@ -56,11 +56,11 @@ class TestLossFunction(unittest.TestCase):
 
     def test_double_bbox(self):
         truth = torch.zeros(TestLossFunction.SHAPE)
-        truth[0, 0, 4] = 1.0        # Bbox confidences
-        truth[0, 0, 9] = 1.0
-        truth[0, 0, -1] = 1.0       # Class
+        truth[0, 0, 0, 4] = 1.0        # Bbox confidences
+        truth[0, 0, 0, 9] = 1.0
+        truth[0, 0, 0, -1] = 1.0       # Class
         pred = torch.zeros(TestLossFunction.SHAPE)
-        pred[0, 0, 0:10] = torch.ones(10)
+        pred[0, 0, 0, 0:10] = torch.ones(10)
         loss_func = SumSquaredErrorLoss()
         result = loss_func(pred, truth)
         self.assertEqual(tuple(result.size()), ())

utils.py
@@ -18,6 +18,7 @@ class SumSquaredErrorLoss(nn.Module):
         self.l_noobj = 0.5
 
     def forward(self, p, a):
+        # print('###################')
         obj_ij = bbox_attr(a, 4) > 0        # Indicator variable, 1 if grid I, bbox J contains object
         noobj_ij = ~obj_ij                  # Opposite, 1's if grid I, bbox J contains NO object
         obj_i = obj_ij[:, :, :, 0:1]        # 1 if grid I has any object at all
@@ -27,24 +28,29 @@ class SumSquaredErrorLoss(nn.Module):
         y_pos = bbox_attr(p, 1) - bbox_attr(a, 1)
         pos_losses = x_pos ** 2 + y_pos ** 2
         # print('pos_losses', pos_losses.size())
+        # print(torch.sum(torch.isnan(pos_losses)).item())
 
         # Bbox dimension losses (prevent negative numbers inside sqrt for predictions)
         width = torch.sqrt(torch.sqrt(bbox_attr(p, 2) ** 2)) - torch.sqrt(torch.sqrt(bbox_attr(a, 2) ** 2))
         height = torch.sqrt(torch.sqrt(bbox_attr(p, 3) ** 2)) - torch.sqrt(torch.sqrt(bbox_attr(a, 3) ** 2))
         dim_losses = width ** 2 + height ** 2
+        # print(torch.sum(torch.isnan(dim_losses)).item())
         # print('dim_losses', dim_losses.size())
 
         # Confidence losses
         confidence_losses = (bbox_attr(p, 4) - torch.ones(obj_ij.size()).to('cuda')) ** 2
         # print('confidence_losses', confidence_losses.size())
+        # print(torch.sum(torch.isnan(confidence_losses)).item())
 
         # Classification losses
         class_losses = (p[:, :, :, 5*config.B:] - a[:, :, :, 5*config.B:]) ** 2
         # print('class_losses', class_losses.size())
+        # print(torch.sum(torch.isnan(class_losses)).item())
 
-        return torch.sum(self.l_coord * (pos_losses[obj_ij] + dim_losses[obj_ij]) + confidence_losses[obj_ij]) \
-               + torch.sum(class_losses[obj_i.expand(class_losses.size())]) \
-               + torch.sum(self.l_noobj * confidence_losses[noobj_ij])
+        total = torch.sum(self.l_coord * (pos_losses[obj_ij] + dim_losses[obj_ij]) + confidence_losses[obj_ij]) \
+                + torch.sum(class_losses[obj_i.expand(class_losses.size())]) \
+                + torch.sum(self.l_noobj * confidence_losses[noobj_ij])
+        return total / config.BATCH_SIZE
 
     def iou(self, a, b):
         pass