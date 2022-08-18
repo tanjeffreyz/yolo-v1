double boxes occurring, fixed height/width loss to have correct gradient using abs and sign, clamping was bad

plot.py
@@ -6,13 +6,13 @@ from models import *
 from torch.utils.data import DataLoader
 
 
-WEIGHTS_PATH = 'models/yolo_v1/08_17_2022/19_05_22/weights/final'
+WEIGHTS_PATH = 'models/yolo_v1/08_18_2022/11_45_46/weights/final'
 
 
 def show_test_images():
     classes = utils.load_class_array()
 
-    dataset = YoloPascalVocDataset('train', normalize=True, augment=True)
+    dataset = YoloPascalVocDataset('test', normalize=True, augment=True)
     loader = DataLoader(dataset, batch_size=config.BATCH_SIZE)
 
     model = YOLOv1ResNet()
@@ -28,7 +28,7 @@ def show_test_images():
                     original[i, :, :, :],
                     predictions[i, :, :, :],
                     classes,
-                    threshold=0.2
+                    threshold=0.1
                 )
 
 

utils.py
@@ -34,52 +34,49 @@ class SumSquaredErrorLoss(nn.Module):
         noobj_ij = ~obj_i               # 1 if no object, all bbox confidences for cells w/o objects should go to 0
 
         # XY position losses
-        x_losses = F.mse_loss(
+        x_losses = mse_loss(
             obj_ij * bbox_attr(p, 0),
-            obj_ij * bbox_attr(a, 0),
-            reduction='sum'
+            obj_ij * bbox_attr(a, 0)
         )
-        y_losses = F.mse_loss(
+        y_losses = mse_loss(
             obj_ij * bbox_attr(p, 1),
-            obj_ij * bbox_attr(a, 1),
-            reduction='sum'
+            obj_ij * bbox_attr(a, 1)
         )
         pos_losses = x_losses + y_losses
         print('pos_losses', pos_losses.item())
 
         # Bbox dimension losses
-        width_losses = F.mse_loss(
-            obj_ij * torch.sqrt(torch.clamp(bbox_attr(p, 2), min=config.EPSILON)),
-            obj_ij * torch.sqrt(torch.clamp(bbox_attr(a, 2), min=config.EPSILON)),
-            reduction='sum'
+        p_width = bbox_attr(p, 2)
+        a_width = bbox_attr(a, 2)
+        width_losses = mse_loss(
+            obj_ij * torch.sign(p_width) * torch.sqrt(torch.abs(p_width) + config.EPSILON),
+            obj_ij * torch.sqrt(a_width)
         )
-        height_losses = F.mse_loss(
-            obj_ij * torch.sqrt(torch.clamp(bbox_attr(p, 3), min=config.EPSILON)),
-            obj_ij * torch.sqrt(torch.clamp(bbox_attr(a, 3), min=config.EPSILON)),
-            reduction='sum'
+        p_height = bbox_attr(p, 3)
+        a_height = bbox_attr(a, 3)
+        height_losses = mse_loss(
+            obj_ij * torch.sign(p_height) * torch.sqrt(torch.abs(p_height) + config.EPSILON),
+            obj_ij * torch.sqrt(a_height)
         )
         dim_losses = width_losses + height_losses
         print('dim_losses', dim_losses.item())
 
         # Confidence losses (target confidence is IOU)
-        obj_confidence_losses = F.mse_loss(
+        obj_confidence_losses = mse_loss(
             obj_ij * bbox_attr(p, 4),
-            obj_ij * max_iou,
-            reduction='sum'
+            obj_ij * max_iou
         )
         print('obj_confidence_losses', obj_confidence_losses.item())
-        noobj_confidence_losses = F.mse_loss(
+        noobj_confidence_losses = mse_loss(
             noobj_ij * bbox_attr(p, 4),
-            0.0 * max_iou,
-            reduction='sum'
+            0.0 * max_iou
         )
         print('noobj_confidence_losses', noobj_confidence_losses.item())
 
         # Classification losses
-        class_losses = F.mse_loss(
+        class_losses = mse_loss(
             obj_i * p[:, :, :, 5*config.B:],
-            obj_i * a[:, :, :, 5*config.B:],
-            reduction='sum'
+            obj_i * a[:, :, :, 5*config.B:]
         )
         print('class_losses', class_losses.item())
 
@@ -135,6 +132,14 @@ def get_iou(p, a):
     intersection[zero_unions] = 0.0
 
     return intersection / union
+
+
+def mse_loss(a, b):
+    return F.mse_loss(
+        torch.flatten(a, end_dim=-2),
+        torch.flatten(b, end_dim=-2),
+        reduction='sum'
+    )
 
 
 def bbox_to_coords(t):